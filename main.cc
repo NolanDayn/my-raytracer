3536f879bd1c5b46d2ddcbb511fd50c3a8799e51
Nolburg <nolandthomas@hotmail.ca>
2023-12-08 21:24:21 -0500

    Chapter 11 Dielectrics

--- main.cc
+++ main.cc
@@ -15,19 +15,21 @@ int main() {
     
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left   = make_shared<dielectric>(color(0.8,1.0,0.7),1.5);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.1);
+    auto material_back   = make_shared<lambertian>(color(0.9,0.1,0.6));
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,  0.0,   -1.0), 0.5, material_center));
+    world.add(make_shared<sphere>(point3( 0.0,  0.0,   -1.0), 0.4, material_center));
     world.add(make_shared<sphere>(point3(-1.0,  0.0,   -1.0), 0.5, material_left));
     world.add(make_shared<sphere>(point3( 1.0,  0.0,   -1.0), 0.5, material_right));
+    world.add(make_shared<sphere>(point3(-0.5,  0.0,   -0.5), 0.5, material_back));
 
     // Camera
     camera cam;
     cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 400;
-    cam.samples_per_pixel = 10;
+    cam.image_width       = 800;
+    cam.samples_per_pixel = 20;
     cam.max_depth         = 20;
     cam.render(world);
 }
